4499d840dcf88b466f2fcd24fb873da460e82c7f
Lyapunov <vaskorichard@gmail.com>
2016-08-07 16:40:26 +0200

    First working version of the photocopy filter.

--- src/cartoonizer.cpp
+++ src/cartoonizer.cpp
@@ -19,26 +19,26 @@ performGimpPhotocopyFilter( const cv::Mat& image, int maskRadius, float treshold
    cv::Mat gray( image.size(), CV_8U);
    cv::cvtColor( image, gray, CV_BGR2GRAY );
 
-   cv::Mat boxFiltered( gray.size(), gray.type() );
-   cv::blur( gray, boxFiltered, cv::Size(2 * blurRadius + 1, 2 * blurRadius + 1) ); 
+   cv::Mat avgBlur( gray.size(), gray.type() );
+   cv::blur( gray, avgBlur, cv::Size(2 * blurRadius + 1, 2 * blurRadius + 1) ); 
 
-   cv::Mat relativeDiff( gray.size(), CV_32F);
-   cv::divide(gray, boxFiltered, relativeDiff);
+   cv::Mat avgMask( gray.size(), gray.type() );
+   cv::blur( gray, avgMask, cv::Size(2 * maskRadius + 1, 2 * maskRadius + 1) ); 
 
    // creating the result per-element, according to the recipe
-   cv::Mat result( gray.size(), CV_8U );
-   boxFiltered.copyTo( result );
+   cv::Mat pixelIntensity( gray.size(), CV_8U );
+   avgBlur.copyTo( pixelIntensity );
 
-   for( int y=0; y < result.rows; y++) {
-      for( int x=0; x < result.cols; x++) {
-         unsigned char& elem = result.at<unsigned char>( y, x );
-         float& reldiff = relativeDiff.at<float>( y, x );
+   for( int y=0; y < pixelIntensity.rows; y++) {
+      for( int x=0; x < pixelIntensity.cols; x++) {
+         unsigned char& elem = pixelIntensity.at<unsigned char>( y, x );
+         float reldiff = static_cast<float>( avgBlur.at<unsigned char>( y, x ) ) / avgMask.at<unsigned char>( y, x );
          if ( reldiff < treshold ) {
-            float result = static_cast<float>(elem) * ( ramp - std::min( ramp, ( treshold - reldiff ) ) );
-            if ( result >= 255.0 ) {
+            float pixelIntensity = static_cast<float>(elem) * ( ramp - std::min( ramp, ( treshold - reldiff ) ) ) / ramp;
+            if ( pixelIntensity >= 255.0 ) {
                elem = 255;
             } else {
-               elem = static_cast<unsigned char>( result );
+               elem = static_cast<unsigned char>( pixelIntensity );
             }
          } else {
             elem = 255;
@@ -46,7 +46,7 @@ performGimpPhotocopyFilter( const cv::Mat& image, int maskRadius, float treshold
       }
    }
 
-   return result;
+   return pixelIntensity;
 }
 
 int main( int argc, char** argv )
@@ -64,7 +64,7 @@ int main( int argc, char** argv )
       return -1;
    }
 
-   cv::Mat photocopy = performGimpPhotocopyFilter( image, 20, 128, 128 );
+   cv::Mat photocopy = performGimpPhotocopyFilter( image, 20, 1, 2 );
 
    cv::namedWindow( "Original", cv::WINDOW_NORMAL );
    cv::namedWindow( "Photocopy", cv::WINDOW_NORMAL );
